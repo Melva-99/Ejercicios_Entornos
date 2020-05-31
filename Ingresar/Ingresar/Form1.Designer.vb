e84c2672db31130de638728c738c36b684f2e2cd
Melva_R <melvarobertina.83@outlook.com>
2020-05-30 19:22:36 -0600

    ventana emergente y ciclo for

---
+++ Ingresar/Ingresar/Form1.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.txtIngresar = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cbmDiv = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cbmMult = New System.Windows.Forms.ComboBox()
+        Me.cbmResta = New System.Windows.Forms.ComboBox()
+        Me.cmbSuma = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnIngresar)
+        Me.GroupBox1.Controls.Add(Me.txtIngresar)
+        Me.GroupBox1.Location = New System.Drawing.Point(25, 24)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(268, 80)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingresar Valor"
+        '
+        'btnIngresar
+        '
+        Me.btnIngresar.Location = New System.Drawing.Point(18, 31)
+        Me.btnIngresar.Name = "btnIngresar"
+        Me.btnIngresar.Size = New System.Drawing.Size(75, 23)
+        Me.btnIngresar.TabIndex = 2
+        Me.btnIngresar.Text = "Ingresar"
+        Me.btnIngresar.UseVisualStyleBackColor = True
+        '
+        'txtIngresar
+        '
+        Me.txtIngresar.Location = New System.Drawing.Point(109, 31)
+        Me.txtIngresar.Multiline = True
+        Me.txtIngresar.Name = "txtIngresar"
+        Me.txtIngresar.Size = New System.Drawing.Size(127, 23)
+        Me.txtIngresar.TabIndex = 1
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnCalcular)
+        Me.GroupBox2.Controls.Add(Me.btnSalir)
+        Me.GroupBox2.Location = New System.Drawing.Point(25, 120)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(268, 80)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "IOpciones"
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(34, 34)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 3
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(134, 34)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 4
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.cbmDiv)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Controls.Add(Me.cbmMult)
+        Me.GroupBox3.Controls.Add(Me.cbmResta)
+        Me.GroupBox3.Controls.Add(Me.cmbSuma)
+        Me.GroupBox3.Location = New System.Drawing.Point(25, 216)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(268, 129)
+        Me.GroupBox3.TabIndex = 1
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Resultado"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(175, 67)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "División"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(31, 67)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Multiplicación"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(166, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Resta"
+        '
+        'cbmDiv
+        '
+        Me.cbmDiv.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbmDiv.FormattingEnabled = True
+        Me.cbmDiv.Location = New System.Drawing.Point(141, 93)
+        Me.cbmDiv.Name = "cbmDiv"
+        Me.cbmDiv.Size = New System.Drawing.Size(95, 21)
+        Me.cbmDiv.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(43, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Suma"
+        '
+        'cbmMult
+        '
+        Me.cbmMult.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbmMult.FormattingEnabled = True
+        Me.cbmMult.Location = New System.Drawing.Point(14, 93)
+        Me.cbmMult.Name = "cbmMult"
+        Me.cbmMult.Size = New System.Drawing.Size(101, 21)
+        Me.cbmMult.TabIndex = 2
+        '
+        'cbmResta
+        '
+        Me.cbmResta.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbmResta.FormattingEnabled = True
+        Me.cbmResta.Location = New System.Drawing.Point(141, 33)
+        Me.cbmResta.Name = "cbmResta"
+        Me.cbmResta.Size = New System.Drawing.Size(95, 21)
+        Me.cbmResta.TabIndex = 1
+        '
+        'cmbSuma
+        '
+        Me.cmbSuma.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbSuma.FormattingEnabled = True
+        Me.cmbSuma.Location = New System.Drawing.Point(18, 33)
+        Me.cmbSuma.Name = "cmbSuma"
+        Me.cmbSuma.Size = New System.Drawing.Size(97, 21)
+        Me.cmbSuma.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(326, 357)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Entrada Texto Message"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnIngresar As Button
+    Friend WithEvents txtIngresar As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cbmDiv As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cbmMult As ComboBox
+    Friend WithEvents cbmResta As ComboBox
+    Friend WithEvents cmbSuma As ComboBox
+End Class
---
+++ Ingresar/Ingresar/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+        Dim valor As Integer
+        valor = InputBox("Ingrese número", "Ingreso")
+        txtIngresar.Text = valor
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Dim opcion As DialogResult
+        opcion = MessageBox.Show("¿Desea salir?", "salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If opcion = DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim suma, resta, multiplicacion, valor As Integer
+        Dim division As Double
+        valor = Val(txtIngresar.Text)
+        For i = 1 To 10 Step 1
+            suma = valor + i
+            resta = valor - i
+            multiplicacion = valor * i
+            division = Format((valor / i).ToString, 2)
+            cmbSuma.Items.Add(valor & " + " & i & "=" & suma)
+            cbmResta.Items.Add(valor & " - " & i & "=" & resta)
+            cbmMult.Items.Add(valor & " x " & i & "=" & multiplicacion)
+            cbmDiv.Items.Add(valor & " / " & i & "=" & division)
+        Next
+
+
+    End Sub
+End Class
