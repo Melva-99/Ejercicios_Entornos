3682d20312f088314adb27c0dbc01a019524fc93
Melva_R <melvarobertina.83@outlook.com>
2020-06-04 14:44:36 -0600

    entrega mascarillas

---
+++ EntregaMascarillas/EntregaMascarillas/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmbdepto = New System.Windows.Forms.ComboBox()
+        Me.btnSolicitar = New System.Windows.Forms.Button()
+        Me.txtDepto = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtStock = New System.Windows.Forms.TextBox()
+        Me.btnEntrega = New System.Windows.Forms.Button()
+        Me.txtmunicipios = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.cmbdepto)
+        Me.GroupBox1.Controls.Add(Me.btnSolicitar)
+        Me.GroupBox1.Controls.Add(Me.txtDepto)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(21, 26)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(276, 272)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Cantidad por departamento"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 135)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(115, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Cant por departamento"
+        '
+        'cmbdepto
+        '
+        Me.cmbdepto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbdepto.FormattingEnabled = True
+        Me.cmbdepto.Location = New System.Drawing.Point(141, 132)
+        Me.cmbdepto.Name = "cmbdepto"
+        Me.cmbdepto.Size = New System.Drawing.Size(129, 21)
+        Me.cmbdepto.TabIndex = 3
+        '
+        'btnSolicitar
+        '
+        Me.btnSolicitar.Location = New System.Drawing.Point(99, 80)
+        Me.btnSolicitar.Name = "btnSolicitar"
+        Me.btnSolicitar.Size = New System.Drawing.Size(95, 23)
+        Me.btnSolicitar.TabIndex = 2
+        Me.btnSolicitar.Text = "Solicitar"
+        Me.btnSolicitar.UseVisualStyleBackColor = True
+        '
+        'txtDepto
+        '
+        Me.txtDepto.Location = New System.Drawing.Point(133, 33)
+        Me.txtDepto.Name = "txtDepto"
+        Me.txtDepto.Size = New System.Drawing.Size(137, 20)
+        Me.txtDepto.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cant. Departamentos"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnSalir)
+        Me.GroupBox2.Controls.Add(Me.txtStock)
+        Me.GroupBox2.Controls.Add(Me.btnEntrega)
+        Me.GroupBox2.Controls.Add(Me.txtmunicipios)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(320, 26)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(231, 272)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'txtStock
+        '
+        Me.txtStock.Location = New System.Drawing.Point(68, 132)
+        Me.txtStock.Name = "txtStock"
+        Me.txtStock.Size = New System.Drawing.Size(100, 20)
+        Me.txtStock.TabIndex = 5
+        '
+        'btnEntrega
+        '
+        Me.btnEntrega.Location = New System.Drawing.Point(85, 80)
+        Me.btnEntrega.Name = "btnEntrega"
+        Me.btnEntrega.Size = New System.Drawing.Size(75, 23)
+        Me.btnEntrega.TabIndex = 5
+        Me.btnEntrega.Text = "Entrega"
+        Me.btnEntrega.UseVisualStyleBackColor = True
+        '
+        'txtmunicipios
+        '
+        Me.txtmunicipios.Location = New System.Drawing.Point(85, 30)
+        Me.txtmunicipios.Name = "txtmunicipios"
+        Me.txtmunicipios.Size = New System.Drawing.Size(100, 20)
+        Me.txtmunicipios.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 33)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Cant municipios"
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(68, 218)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 6
+        Me.btnSalir.Text = "Salir" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(637, 310)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cmbdepto As ComboBox
+    Friend WithEvents btnSolicitar As Button
+    Friend WithEvents txtDepto As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnEntrega As Button
+    Friend WithEvents txtmunicipios As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtStock As TextBox
+    Friend WithEvents btnSalir As Button
+End Class
---
+++ EntregaMascarillas/EntregaMascarillas/Form1.vb
@@ -0,0 +1,157 @@
+﻿Public Class Form1
+
+    Private Sub btnSolicitar_Click(sender As Object, e As EventArgs) Handles btnSolicitar.Click
+        'for, dowhile
+        Dim departamentos, mascarillas As Integer
+
+        departamentos = Val(txtDepto.Text)
+
+        If departamentos > 18 Or departamentos < 0 Then
+            MessageBox.Show("El numero de departamentos no puede ser mayor que 0 ni menor que 18 ", "Departamentos", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtDepto.Clear()
+            txtDepto.Focus()
+            Exit Sub
+        End If
+
+        For i = 1 To departamentos Step 1
+            Do
+                Try
+                    mascarillas = InputBox("Ingrese la cantidad de mascarillas(Entre 1 - 10,000)" & i, ", Ingresar")
+                    If mascarillas = vbAbort Then
+                        Exit Sub
+                    End If
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+
+                    mascarillas = -1
+
+                End Try
+
+            Loop While (mascarillas < 0 Or mascarillas > 10000)
+            cmbdepto.Items.Add(mascarillas)
+
+
+        Next
+
+    End Sub
+
+    Private Sub btnEntrega_Click(sender As Object, e As EventArgs) Handles btnEntrega.Click
+        Dim cantidadDepartamento, cantidadEntregada As Integer
+        Dim idDepartamento As Integer
+
+        cantidadDepartamento = cmbdepto.SelectedItem
+        idDepartamento = cmbdepto.SelectedIndex
+
+        'MsgBox("Numero de municipios: " & numeroMunicipios & "Cantidad asignada a departamentos: " & cantidadDepartamento & "ID departamento: " & idDepartamento)
+
+        Select Case idDepartamento
+            Case 0
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 8)
+            Case 1
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 10)
+            Case 2
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 21)
+            Case 3
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 23)
+            Case 4
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 12)
+            Case 5
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 16)
+            Case 6
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 19)
+            Case 7
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 28)
+            Case 8
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 6)
+            Case 9
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 17)
+            Case 10
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 4)
+            Case 11
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 19)
+            Case 12
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 28)
+            Case 13
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 16)
+            Case 14
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 23)
+            Case 15
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 28)
+            Case 16
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 9)
+            Case 17
+                cantidadEntregada = EntregaMascarillas(cantidadDepartamento, 11)
+            Case Else
+                MessageBox.Show("Error, consulte con el encargado", "Entrega de Municipios", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Select
+        txtStock.Text = cantidadEntregada
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbdepto.DropDownStyle = ComboBoxStyle.DropDownList
+        txtDepto.Focus()
+    End Sub
+
+    Private Sub cmbdepto_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbdepto.SelectedIndexChanged
+        Dim idDepartamento As Integer
+        idDepartamento = cmbdepto.SelectedIndex
+        txtStock.Text = cmbdepto.SelectedItem
+        Select Case idDepartamento
+            Case 0
+                txtmunicipios.Text = 8
+            Case 1
+                txtmunicipios.Text = 10
+            Case 2
+                txtmunicipios.Text = 21
+            Case 3
+                txtmunicipios.Text = 23
+            Case 4
+                txtmunicipios.Text = 12
+            Case 5
+                txtmunicipios.Text = 16
+            Case 6
+                txtmunicipios.Text = 19
+            Case 7
+                txtmunicipios.Text = 28
+            Case 8
+                txtmunicipios.Text = 6
+            Case 9
+                txtmunicipios.Text = 17
+            Case 10
+                txtmunicipios.Text = 4
+            Case 11
+                txtmunicipios.Text = 19
+            Case 12
+                txtmunicipios.Text = 28
+            Case 13
+                txtmunicipios.Text = 16
+            Case 14
+                txtmunicipios.Text = 23
+            Case 15
+                txtmunicipios.Text = 28
+            Case 16
+                txtmunicipios.Text = 9
+            Case 17
+                txtmunicipios.Text = 11
+            Case Else
+                MessageBox.Show("Error, consulte con el encargado", "Entrega de Municipios", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Select
+    End Sub
+    Private Function entregaMascarillas(stock As Integer, cantidadMunicipios As Integer) As Integer
+        Dim entregadas As Integer
+        For i = 1 To cantidadMunicipios Step 1
+            If (stock <= 0) Then
+                MessageBox.Show("No quedan mascarillas en inventario. ", "Entrega de Mascarillas", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Return 0
+            End If
+            entregadas = InputBox("Ingrese la cantidad de mascarillas para este Municipio ", "Asignacion por Municipios")
+            stock -= entregadas
+            txtStock.Text = stock
+        Next
+        Return stock
+    End Function
+
+    Private Sub btnSalir_Click_1(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+End Class
